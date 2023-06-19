18e09352dd7405a24548101bea80087118fe4ffd
tyronemurg <tyronemurg@gmail.com>
2023-06-19 12:27:08 +0200

    Remove http client from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -9,17 +8,9 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent implements OnInit {
   posts : any;
  
-  constructor(private http: HttpClient){}
+  constructor(){}
  
   ngOnInit(): void{
-    this.http.get('http://localhost/your_wp_api/wp-json/wp/v2/posts').subscribe(data =>{
-      // for(let key in data){
-      //   if(data.hasOwnProperty(key)){
-      //     this.posts.push(data[key]);
-      //   }
-      // }
-      this.posts = data;
-      console.log(data);
-    })
+
   }
 }
