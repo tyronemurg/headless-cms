36edb44394c84622b34502625a5874c8520cda89
tyronemurg <tyronemurg@gmail.com>
2023-06-27 13:24:45 +0200

    Blog Category | REST API

--- src/app/pages/blog-categories/blog-categories.component.ts
+++ src/app/pages/blog-categories/blog-categories.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-blog-categories',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogCategoriesComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  posts: any;
+ 
+  constructor(private http: HttpClient){}
+ 
+  ngOnInit(): void{
+    this.http.get('http://localhost/webwarrior/wp-json/wp/v2/posts/?_embed/&categories=34').subscribe(data =>{
+      // for(let key in data){
+      //   if(data.hasOwnProperty(key)){
+      //     this.posts.push(data[key]);
+      //   }
+      // }
+      this.posts = data;
+      console.log(this.posts);
+    })
   }
 
 }
