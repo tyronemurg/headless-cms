70f9b89ef1af72abf45682e9cbeb7f990bf07bb8
tyronemurg <tyronemurg@gmail.com>
2023-06-29 12:26:14 +0200

    Product Service | Single Product WC

--- src/app/services/env.service.ts
+++ src/app/services/env.service.ts
@@ -8,9 +8,10 @@ export class EnvService {
   post_single_url = 'http://localhost/webwarrior/wp-json/wp/v2/posts'
   post_url = 'http://localhost/webwarrior/wp-json/wp/v2/posts?_embed/'
 
-  base_url = 'https://your_wordpress/wp-json/wc/v3/products?'
+  product_single_url = 'http://localhost/webwarrior/wp-json/wc/v3/products'
+  base_url = 'http://localhost/webwarrior/wp-json/wc/v3/products?'
   ck = 'ck_your_consumer_key';
-  cs = 'cs_your_consumer_secret';
+  cs = 'cs_your_cosnumer_secret';
 
   PRODUCT_API_URL = `${this.base_url}consumer_key=${this.ck}&consumer_secret=${this.cs}`;
   constructor() { }
