ed7a40456aa33aeac1b4b7cbf26a27c5ac9f3e9d
tyronemurg <tyronemurg@gmail.com>
2023-07-09 09:09:02 +0200

    namespace

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/lp'),
+      dir: require('path').join(__dirname, './coverage/headless-cms'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'lp'`, () => {
+  it(`should have as title 'headless-cms'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('headless-cms');
